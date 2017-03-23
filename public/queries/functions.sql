e1eef5884cbb3fc2d3854b3a4ddcbad84b0814e5
Kevin Marete <kevomarete@gmail.com>
2017-03-23 14:18:48 +0300

    #Fix: Calculation for AMC

--- public/queries/functions.sql
+++ public/queries/functions.sql
@@ -5,9 +5,10 @@ CREATE OR REPLACE FUNCTION fn_get_national_amc(pm_drug_id integer, pm_period_dat
 BEGIN
     DECLARE amc INT(10);
 
-    SELECT AVG(total) INTO amc 
+    SELECT (SUM(total)/6) INTO amc 
     FROM tbl_facility_consumption
 	WHERE CONCAT(CONCAT_WS('-', period_year, DATE_FORMAT(str_to_date(period_month,'%b'), '%m')), '-01') >= date_sub(pm_period_date, interval 6 month)
+	AND CONCAT(CONCAT_WS('-', period_year, DATE_FORMAT(str_to_date(period_month,'%b'), '%m')), '-01') <= pm_period_date
 	AND drug_id = pm_drug_id;
 
  	RETURN (amc);
