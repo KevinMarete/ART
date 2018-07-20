9ee3cc8e73f547053e61c15a2bcd8aa81c958e85
kariukye <kariukijoni@gmail.com>
2018-07-20 12:55:30 +0300

    Attached ftp css and js

--- application/modules/Ftp/views/elfinder.php
+++ application/modules/Ftp/views/elfinder.php
@@ -1,29 +1,25 @@
 <!DOCTYPE html>
 <html>
-
     <head>
         <meta charset="utf-8">
         <title>elFinder 2.1.x source version with PHP connector</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2" />
-
+        <!--favicon-->
+        <link rel="shortcut icon" type="text/css" href="<?php echo base_url() . 'public/ftp/img/favicon.ico'; ?>">  
         <!-- jQuery and jQuery UI (REQUIRED) -->
-        <link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'public/ftp/file_manager/libs/css/jquery/jquery-ui.css'; ?>"> 
+        <script type="text/javascript" src="<?php echo base_url() . 'public/ftp/file_manager/libs/js/jquery/jquery.min.js'; ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url() . 'public/ftp/file_manager/libs/js/jquery/jquery-ui.min.js'; ?>"></script>
         <!-- elFinder CSS (REQUIRED) -->
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'public/ftp/file_manager/css/elfinder.min.css'; ?>">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'public/ftp/file_manager/css/theme.css'; ?>">
 
         <!-- elFinder JS (REQUIRED) -->
         <script src="<?php echo base_url() . 'public/ftp/file_manager/js/elfinder.min.js'; ?>"></script>
-
         <!-- GoogleDocs Quicklook plugin for GoogleDrive Volume (OPTIONAL) -->
         <!--<script src="js/extras/quicklook.googledocs.js"></script>-->
-
         <!-- elFinder translation (OPTIONAL) -->
         <!--<script src="js/i18n/elfinder.ru.js"></script>-->
-
         <!-- elFinder initialization (REQUIRED) -->
         <?php echo "<script>path = '" . base_url() . "'</script>" ?>
         <script type="text/javascript" charset="utf-8">
@@ -38,7 +34,9 @@
         </script>
     </head>
     <body>
-        <!-- Element where elFinder will be created (REQUIRED) -->
-        <div id="elfinder"></div>
+        <div class="container-fluid">
+            <!-- Element where elFinder will be created (REQUIRED) -->
+            <div id="elfinder"></div>
+        </div>
     </body>
 </html>
